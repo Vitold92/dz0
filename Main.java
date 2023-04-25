59f29448cc4ecd3305d4a0eb7d37bae4f9b6268f
Vitold92 <vitold1992@gmail.com>
2023-04-25 17:11:10 +0200

    Created function that parses string removing the duplicated characters

--- Main.java
+++ Main.java
@@ -1,20 +1,47 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {2, 4, 6, 7, 8, 9, 10};
-        int myVar = Main.sumOfEvenNumber(numbers);
-        System.out.println(myVar);
+        int sumOfEvenNumber = Main.sumOfEvenNumber(numbers);
+        System.out.println("Sum of even numbers:");
+        System.out.println();
+        System.out.println(sumOfEvenNumber);
+        System.out.println();
+
+        String stringWithDuplicatedCharacters = "Alexx9800";
+        Main.printStringCharacters(stringWithDuplicatedCharacters);
     }
 
     public static int sumOfEvenNumber(int[] numbers) {
-        int sum = 0;
+        int sumOfNumbers = 0;
 
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] % 2 == 0) {
-                sum += numbers[i];
+                sumOfNumbers += numbers[i];
             }
         }
 
-        return sum;
+        return sumOfNumbers;
+    }
+
+    public static void printStringCharacters(String value) {
+        char[] characters = value.toCharArray();
+
+        ArrayList<Character> charactersList = new ArrayList<Character>();
+
+        for (int i = 0; i < characters.length; i++) {
+            if (!charactersList.contains(characters[i])) {
+                charactersList.add(characters[i]);
+            }
+        }
+
+        System.out.println("Parsed string without duplicated characters:");
+        System.out.println();
+
+        for (int i = 0; i < charactersList.size(); i++) {
+            System.out.println(charactersList.get(i));
+        }
     }
 }
 
